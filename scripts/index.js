b43070fd6e2375b83f3f0aaa2d4c45bd3e3a5c46
MustakimMRidoy <hmmustakimbillahridoy@gmail.com>
2025-07-24 06:17:52 +0600

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -432,7 +432,7 @@
                     </div>
                 </div>
                 <div class="window-content">
-                    <iframe src="${page}" scrolling="yes" loading="lazy" onload="handleIframeLoad(this, '${windowId}', '${title}')" onerror="handleIframeError(this, '${windowId}', '${title}')" style="width:100%; height:100%; border:none;" sandbox="allow-scripts allow-forms"></iframe>
+                    <iframe src="${page}" sandbox="allow-scripts allow-forms" scrolling="yes" loading="lazy" onload="handleIframeLoad(this, '${windowId}', '${title}')" onerror="handleIframeError(this, '${windowId}', '${title}')" style="width:100%; height:100%; border:none;"></iframe>
                 </div>
                 <div class="window-resize-handle"></div>
             `;

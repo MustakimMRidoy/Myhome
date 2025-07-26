039010b407410637ba70b2e640e9deab3f6e76e1
MustakimMRidoy <hmmustakimbillahridoy@gmail.com>
2025-07-26 22:16:48 +0600

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -10,17 +10,18 @@
             { id: 'security', name: 'Security', icon: 'fas fa-shield-alt', color: '#d4006f', page: 'systemApps/security.html(pending)', type: 'System', pinned: false },
             { id: 'calculator', name: 'Calculator', icon: 'fas fa-calculator', color: '#5c2d91', page: 'systemApps/calculator.html', type: 'System', pinned: false },
             { id: 'notes', name: 'Notepad', icon: 'fas fa-sticky-note', color: '#00bcf2', page: 'systemApps/note.html', type: 'System', pinned: false },
+            { id: 'networkStatus', name: 'Network Status', icon: 'fas fa-network-wired', color: '#0078d4', page: 'systemApps/networkStatus.html', type: 'System', pinned: false },
             { id: 'blasterGame', name: 'Blaster Game', icon: 'fas fa-gamepad', color: '#107c10', page: 'installedApps/blasterGame.html', type: 'Game', pinned: false },
             { id: 'puzzle', name: 'Puzzle Game', icon: 'fas fa-puzzle-piece', color: '#ff4b4b', page: 'installedApps/puzzle.html', type: 'Game', pinned: false },
             { id: 'tools', name: 'System Tools', icon: 'fas fa-tools', color: '#795548', page: 'installedApps/tools.html', type: 'Utility', pinned: false },
-            { id: 'imageEditor', name: 'Paint', icon: 'fas fa-paint-brush', color: '#e3008c', page: 'installedApps/imageEditor.html', type: 'Creative', pinned: false },
-            { id: 'networkStatus', name: 'Network Status', icon: 'fas fa-network-wired', color: '#0078d4', page: 'systemApps/networkStatus.html', type: 'System', pinned: false },
 	    { id: 'ColorMaster', name: 'Color Master', icon: 'fas fa-eye-dropper', color: '#26E974', page: 'https://mustakimridoymr.github.io/ColorMaster.html', type: 'Utility', pinned: false },
 	    { id: 'adminPanel', name: 'Admin Panel', icon: 'fas fa-tachometer-alt', color: '#5779A4', page: 'https://mustakimridoymr.github.io/Admin/index.html', type: 'Utility', pinned: false },
+            { id: 'imageEditor', name: 'Paint', icon: 'fas fa-paint-brush', color: '#e3008c', page: 'installedApps/imageEditor.html', type: 'Creative', pinned: false },
 	    { id: 'CoinEarnerPro', name: 'Coin Earner Pro', icon: 'fas fa-coins', color: '#FF8F00', page: 'https://dailyearningmony.netlify.app', type: 'Creative', pinned: false },
 	    { id: 'jobSearch', name: 'Job Search portal', icon: 'fas fa-briefcase', color: '#A40C99', page: 'https://mustakimridoymr.github.io/Jobs.html', type: 'Internet', pinned: false },
 	    { id: 'news', name: 'Global News Hub Pro', icon: 'fas fa-rss', color: '#0056FF', page: 'https://mustakimridoymr.github.io/newsApp.html', type: 'Internet', pinned: false },
-	    { id: 'AmazonAppstoreCollection', name: 'Amazon Appstore Collection', icon: 'fab fa-amazon', color: '#FFFFFF', page: 'https://amzn.to/3NOG3er', type: 'Internet', pinned: false }
+	    { id: 'AmazonAppstoreCollection', name: 'Amazon Appstore Collection', icon: 'fab fa-amazon', color: '#FFFFFF', page: 'https://amzn.to/3NOG3er', type: 'Internet', pinned: false },
+	    { id: 'APKPureMarketplace', name: 'APKPure Marketplace', icon: 'fas fa-mobile-alt', color: '#008E3E', page: 'https://apkpure.com/developer?id=23399507', type: 'Internet', pinned: false }
         ];
         // Background Images
         const backgrounds = [

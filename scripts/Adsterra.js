8caa3a6bcad3b318d38f54853f8c2ef789c650bb
MustakimMRidoy <hmmustakimbillahridoy@gmail.com>
2025-07-27 08:40:08 +0600

    Update Adsterra.js

--- scripts/Adsterra.js
+++ scripts/Adsterra.js
@@ -1,1 +1,3 @@
-<script type='text/javascript' src='//pl27249870.profitableratecpm.com/fa/94/5d/fa945da78d4bef9d1cc45b73fca22fb9.js'></script>
+    <script type='text/javascript' src='//pl27270239.profitableratecpm.com/f0/ef/44/f0ef448af73bd4e1b67a1759227b2e03.js'></script>
+    <script async="async" data-cfasync="false" src="//pl27246921.profitableratecpm.com/86f295a00166ddfdf3c7bdb7c13ce400/invoke.js"></script>
+    <script type='text/javascript' src='//pl27249870.profitableratecpm.com/fa/94/5d/fa945da78d4bef9d1cc45b73fca22fb9.js'></script>

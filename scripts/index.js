f3eefe775800025405ff2e4c12c3b84fd3c08b2a
MustakimMRidoy <hmmustakimbillahridoy@gmail.com>
2025-07-27 08:56:01 +0600

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -546,9 +546,6 @@ if (windowId.includes('Ads')) {
                 <div class="window-content">
 		    <div id="bannerAd" class="ad-container"></div>
                     <iframe src="${page}"
-                            sandbox="allow-scripts allow-forms"
-                            loading="lazy"
-                            referrerpolicy="no-referrer"
                             onload="handleIframeLoad(this, '${windowId}', '${title}')"
                             onerror="handleIframeError(this, '${windowId}')">
                     </iframe>
